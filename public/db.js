4b29a6751f2dabbd29f6730a0d9d02a0806dd2dd
Valerie Russell <vruss14@gmail.com>
2021-05-27 15:37:44 -0400

    rewrote the onupgradeneeded function to update version number and create only one object store

--- public/db.js
+++ public/db.js
@@ -1,12 +1,27 @@
-const request = indexedDB.open("budgetDB", 1);
+let db;
+let budgetVersion;
 
-request.onupgradeneeded = ({ target }) => {
-    const db = target.result;
+// Requests to create a database for the application in IndexedDB
+// The version is either the budgetVersion or 13
+const request = indexedDB.open('BudgetDB', budgetVersion || 13);
 
-    const objectStore = db.createObjectStore("Transactions");
-    objectStore.createIndex("Deposit", "Deposit");
-    objectStore.createIndex("Expense", "Expense");
-  };
+request.onupgradeneeded = function (event) {
+  console.log('IndexedDB needs to be updated.');
+
+  // Updates the version number based on event info
+  const { oldVersion } = event;
+  const newVersion = event.newVersion || db.version;
+
+  console.log(`The database has been updated from version ${oldVersion} to ${newVersion}`);
+
+  db = event.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+
+    // This object store is viewable within the BudgetDB in DevTools
+    db.createObjectStore('BudgetStorage', { autoIncrement: true });
+  }
+};
   
 request.onsuccess = event => {
   console.log(request.result);
